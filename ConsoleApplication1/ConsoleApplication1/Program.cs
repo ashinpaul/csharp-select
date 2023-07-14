566ab3f30a7097303a0caa5b4a5e081804627d7d
ashin paul <ashinpaul46@gmail.com>
2023-07-14 09:30:53 +0530

    ok

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        private static object user;
+
+        static void Main(string[] args)
+        {
+            string connectorString = "server=localhost";user = root;database = Sys;port = 3306;password = rajagiri;
+            string query = "select *from mytable 50";
+
+            try
+            {
+                connection.open();
+                adapter.Fill(dataSet, "mytable50");
+                DataTable datatable = dataSet.Tables["mytable50"];
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    int id = (int)row["id"];
+                    string name = (string)row["Sname"];
+                    Console.WriteLine($"ID:{id},Name:{name}");
+                }
+            }catch(Exception e) { Console.WriteLine(e); }
+        }
+    }
+}
